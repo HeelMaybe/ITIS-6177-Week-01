abf24b0018fcf82de2ab323516dd7425b87490bf
HeelMaybe <ghelton1@uncc.edu>
2022-01-16 16:05:13 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World23!");
+});
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
